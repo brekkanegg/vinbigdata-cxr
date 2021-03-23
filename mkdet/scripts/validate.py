158287affbc1fdef225c94d9cfbe26d9b6b6558b
brekkanegg-vuno <brekkanegg@vuno.co>
2021-03-23 14:24:22 +0900

    add one more class

--- mkdet/scripts/validate.py
+++ mkdet/scripts/validate.py
@@ -278,7 +278,7 @@ class Validator(object):
         dths = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
 
         cls_th_combi = []
-        for c in tqdm(range(14)):
+        for c in tqdm(range(15)):
             c_vineval = copy.deepcopy(self.vineval)
             c_vineval.annotations["categories"] = [
                 c_vineval.annotations["categories"][c]
